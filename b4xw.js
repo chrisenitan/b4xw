d9a4eef162b9c62d2957e2e2444b867416ea7bb0
chrisenitan <enitanchris@gmail.com>
2024-04-07 12:44:25 +0200

    chore(updates): compress

--- b4xw.js
+++ b4xw.js
@@ -1,12 +1,12 @@
 //Gets tab count from sw and checks before closing if tab is the last in window
 //requires at least one interaction with the page after load event is fired
 window.addEventListener("load", function () {
-  var port = chrome.runtime.connect({ name: "contentScriptConnection" })
+  const port = chrome.runtime.connect({ name: "contentScriptConnection" })
   port.postMessage({ message: "Registering tab with background" })
   port.onMessage.addListener(function (message) {
-    const { totalTabs } = message
-    if (totalTabs == 1) {
-      console.log(`Adding unload event to last tab ${totalTabs}`)
+    const { tabsLength } = message
+    if (tabsLength == 1) {
+      console.log(`Adding unload event to last tab ${tabsLength}`)
       window.addEventListener("beforeunload", function (event) {
         event.preventDefault()
         return ""
