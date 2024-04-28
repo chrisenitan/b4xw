b95f5923aaf8c8f4a1607207a7ead175bbaa4fdb
chrisenitan <enitanchris@gmail.com>
2024-04-28 19:34:37 +0200

    chore(updates): clean up

--- b4xw.js
+++ b4xw.js
@@ -1,21 +1,13 @@
-//b4xw.js https://developer.chrome.com/docs/extensions/reference/api
-/**
- * Returns true if current tab contains at least 2 history states
- * Ideally, tab is old and has been used vs new tab
- */
-const useUnloader = () => {
-  const history = window.history.length
-  return history > 2
-}
-
-//requires sticky activation https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event#usage_notes
+//Gets tab count from sw and checks before closing if tab is the last in window or essentially the first tab opened
+//requires sticky activation https://developer.mozilla.org/en-US/docs/Glossary/Sticky_activation
 window.addEventListener("load", function () {
   const port = chrome.runtime.connect({ name: "contentScriptConnection" })
   port.postMessage({ message: "Registering tab with background" })
   port.onMessage.addListener(function ({ tabsLength }) {
     if (tabsLength == 1) {
+      console.log(`Adding unload event to last tab ${tabsLength}`)
       window.addEventListener("beforeunload", function (event) {
-        if (useUnloader()) event.preventDefault()
+        event.preventDefault()
         return ""
       })
     }
--- background.js
+++ background.js
@@ -1,13 +1,10 @@
-//Get the number of currently opened tabs for content script page
+//Get number of currently opened tabs and send back to active content script page
 chrome.runtime.onConnect.addListener(function (port) {
   if (port.name === "contentScriptConnection") {
     console.log("Connected to content script")
     port.onMessage.addListener(function (message) {
-      chrome.tabs.onUpdated.addListener(function (tabId, removeInfo) {
-        port.postMessage({ tabsLength: 1, tabs: tabId })
-      })
       chrome.tabs.query({ currentWindow: true }, function (tabs) {
-        port.postMessage({ tabsLength: tabs.length, tabs: tabs })
+        port.postMessage({ tabsLength: tabs.length })
       })
     })
   }
