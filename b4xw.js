ef157d7761903aa7e66d6b84a8103e8e92c28982
chrisenitan <enitanchris@gmail.com>
2024-04-28 23:18:41 +0200

    chore(updates): safe exit for events

--- b4xw.js
+++ b4xw.js
@@ -1,15 +1,20 @@
-//Gets tab count from sw and checks before closing if tab is the last in window or essentially the first tab opened
+//Gets tab count from sw and checks before closing if tab is the last in window
 //requires sticky activation https://developer.mozilla.org/en-US/docs/Glossary/Sticky_activation
+
+const beforeUnload = (event) => {
+  event.preventDefault()
+  return ""
+}
+
 window.addEventListener("focus", function () {
   const port = chrome.runtime.connect({ name: "contentScriptConnection" })
-  port.postMessage({ message: "Registering tab with background" })
+  port.postMessage({ message: "Registering tab with background script" })
   port.onMessage.addListener(function ({ tabsLength }) {
     if (tabsLength == 1) {
       console.log(`Adding unload event to last tab ${tabsLength}`)
-      window.addEventListener("beforeunload", function (event) {
-        event.preventDefault()
-        return ""
-      })
+      window.addEventListener("beforeunload", beforeUnload)
+    } else {
+      window.removeEventListener("beforeunload", beforeUnload)
     }
   })
 })
